b637633b9006a2ca6166d44fbdda03d7f0e45b7d
Kaushal55555 <lordqshal@gmail.com>
2025-11-18 11:48:07 +0545

    pushing task 2 and Task 3

---
+++ Task1/ParameterDemo.cs
@@ -0,0 +1,22 @@
+﻿public class ParameterDemo
+{
+    public void Increase(ref int number)
+    {
+        number += 10;
+    }
+
+    public void GetFullName(out string fullname)
+    {
+        fullname = "Kaushal Khadka";
+    }
+
+    public int SumAll(params int[] numbers)
+    {
+        int sum = 0;
+        foreach (int n in numbers)
+        {
+            sum += n;
+        }
+        return sum;
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -21,5 +21,30 @@ class Program
         Console.WriteLine($"Student 2: {s2.name}, {s2.age}, {s2.course}");
         Console.WriteLine("College: " + Student.college);
 
+        Console.WriteLine("\n========== TASK 2 ==========");
+
+        // Task 2: Methods
+        Calculator calc = new Calculator();
+        calc.PrintWelcome();
+        Console.WriteLine("Add: " + calc.Add(10, 20));
+        Console.WriteLine("Multiply default: " + calc.Multiply(5));
+        Console.WriteLine("Multiply(5,3): " + calc.Multiply(5, 3));
+
+        Console.WriteLine("\n========== TASK 3 ==========");
+
+        // Task 3: Parameter Types
+        ParameterDemo pd = new ParameterDemo();
+
+        int number = 20;
+        pd.Increase(ref number);
+        Console.WriteLine("Increase(ref): " + number);
+
+        pd.GetFullName(out string fname);
+        Console.WriteLine("Full name: " + fname);
+
+        Console.WriteLine("SumAll: " + pd.SumAll(1, 2, 3, 4, 5));
+
+        Console.WriteLine("\n========== TASK 4 ==========");
+
     }
 }

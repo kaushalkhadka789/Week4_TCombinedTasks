c0d7b0b7f9bf878a7af19599e6b75b26eba980d6
Kaushal55555 <lordqshal@gmail.com>
2025-11-18 11:40:52 +0545

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("========== TASK 1 ==========");
+
+        // Task 1: Class, Objects & Fields
+        Student s1 = new Student();
+        s1.name = "Kaushal";
+        s1.age = 22;
+        s1.course = "Computing";
+
+        Student s2 = new Student();
+        s2.name = "Suman";
+        s2.age = 21;
+        s2.course = "Cyber Security";
+
+        Console.WriteLine($"Student 1: {s1.name}, {s1.age}, {s1.course}");
+        Console.WriteLine($"Student 2: {s2.name}, {s2.age}, {s2.course}");
+        Console.WriteLine("College: " + Student.college);
+
+    }
+}
---
+++ Task1/Student.cs
@@ -0,0 +1,8 @@
+﻿public class Student
+{
+    public string name;
+    public int age;
+    public string course;
+
+    public static string college = "Moon Beam Academy";
+}
